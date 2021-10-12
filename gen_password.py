7ad6506001199037370dd884eb0134f940b39d15
DiegoMao2077 <56417633+DiegoMao2077@users.noreply.github.com>
2021-10-12 13:09:56 -0500

    Add files via upload

---
+++ gen_password.py
@@ -0,0 +1,20 @@
+import random
+
+def generator_password():
+    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+    caracter = MAYUS + MINUS + NUMS + CHARS
+    password=[]
+    for i in range(20):
+        caracter_random = random.choice(caracter)
+        password.append(caracter_random)
+
+    password = "".join(password)
+    return password
+def main():
+    password = generator_password()
+    print("Your new password is: " + password)
+main()
